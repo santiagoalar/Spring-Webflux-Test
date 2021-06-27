520bda69075a066d58905f555291b3ed4278f131
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-27 16:14:40 -0500

    First commit

---
+++ src/test/java/com/example/Spring/Webflux/Test/UppercaseConverterTest.java
@@ -0,0 +1,22 @@
+package com.example.Spring.Webflux.Test;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UppercaseConverterTest {
+
+    final TestPublisher<String> testPublisher = TestPublisher.create();
+
+    @Test
+    void testUpperCase() {
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
+    }
+
+}
